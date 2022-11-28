1373f9d75bfe3358bcb549d53eea996e05121042
Michael <greenmr95@hotmail.com>
2022-11-28 05:10:42 +0000

    Notes

--- index.js
+++ index.js
@@ -1,29 +1,63 @@
-let balance = 500.00;
 
-class Withdrawal {
+//Not yet completed the two stretch features.
 
-  constructor(amount) {
-    this.amount = amount;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
   }
-
-  commit() {
-    balance -= this.amount;
-  }
-
 }
 
+class Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account
+  }
+  commit() {
+    this.account.balance += this.value;
+  }
+}
 
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount
+  }
+}
 
+class Deposit extends Transaction {
+  get value() {
+    return this.amount
+  }
+}
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account('Broadsword');
 
-t1 = new Withdrawal(50.25);
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+console.log("Transaction 1:", t1.value);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+console.log("Transaction 2:", t2.value);
 
-console.log('Balance:', balance);
+console.log('Ending Balance:', myAccount.balance);
+console.log(myAccount);
+
+// const myAccount = new Account("snow-patrol");
+
+// t1 = new Withdrawal(50.25, myAccount);
+// t1.commit();
+// // console.log(t1.value);
+// console.log('Transaction 1:', t1);
+
+// t2 = new Withdrawal(9.99, myAccount);
+// t2.commit();
+// console.log('Transaction 2:', t2);
+
+// t3 = new Deposit(61.23, myAccount)
+// t3.commit()
+// console.log('Transaction 3:', t3);
+
+// console.log('Balance:', myAccount.balance.toFixed(2));
